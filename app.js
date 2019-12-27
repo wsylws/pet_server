46f8a8cc645f758d628b1ea2bf5867cb5da3a906
wsylws <18859606993@163.com>
2019-12-27 21:50:30 +0800

    finish

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// app.listen(3000, function () {
-//     console.log('open sever successful, listen in port: 3000')
-// })
+app.listen(3000, function () {
+    console.log('open sever successful, listen in port: 3000')
+})
 
 module.exports = app;
